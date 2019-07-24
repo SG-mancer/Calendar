99690ef6a2aaed708c11278996fc3b4605a90401
t3liph0n3 <hyperstn@outlook.jp>
2019-07-24 19:54:59 +0800

    colour change

--- organiser.js
+++ organiser.js
@@ -5,7 +5,7 @@ makeCal();
 function count100Days(){
    var dt1 = new Date(); //set two variables to compare.
    var dt0 = new Date();
-   dt0.setFullYear(2019, 6, 22); //Day 0 of 100 days of X (is 28 Feb 2019)
+   dt0.setFullYear(2019, 6, 30); //Day 0 of 100 days of X (is 29 Jul 2019)
    var dayCount = Math.floor((Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) - Date.UTC(dt0.getFullYear(), dt0.getMonth(), dt0.getDate()))/(1000*60*60*24));
 
    document.getElementById("daysCount").innerHTML = dayCount;
